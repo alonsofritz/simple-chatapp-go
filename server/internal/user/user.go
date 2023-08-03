d45e9d0d10274956df52440a4283d26415e25760
Alonso Fritz <alonso.fritz@ecocentauro.com.br>
2023-08-03 15:22:04 -0300

    feat: implement UserReq and UserRes and Service interface

--- server/internal/user/user.go
+++ server/internal/user/user.go
@@ -9,6 +9,22 @@ type User struct {
 	Password string `json:"password"`
 }
 
+type CreateUserReq struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type CreateUserRes struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
 type UserRepository interface {
 	CreateUser(ctx context.Context, user *User) (*User, error)
+}
+
+type Service interface {
+	CreateUser(c context.Context, req *CreateUserReq) (*CreateUserRes, error)
 }
