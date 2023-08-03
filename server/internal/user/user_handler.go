4d8b333e8346966c1db235be7e3d0b34e986ce9a
Alonso Fritz <alonso.fritz@ecocentauro.com.br>
2023-08-03 15:23:01 -0300

    wip-feat: user_handler created

---
+++ server/internal/user/user_handler.go
@@ -0,0 +1,18 @@
+// TODO: Implement this handler
+package user
+
+import "github.com/gin-gonic/gin"
+
+type Handler struct {
+	Service
+}
+
+func NewHandler(s Service) *Handler {
+	return &Handler{
+		Service: s,
+	}
+}
+
+func (h *Handler) CreateUser(c *gin.Context) {
+
+}
