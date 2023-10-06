0dad9aa2b4a6e8468545be7fc9e8d1e9ca9c2e07
Alonso Fritz <alonso.fritz@ecocentauro.com.br>
2023-10-06 08:38:47 -0300

    build: add router with gin

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -4,11 +4,20 @@ import (
 	"log"
 
 	"github.com/alonsofritz/simple-chatapp-go/server/db"
+	"github.com/alonsofritz/simple-chatapp-go/server/internal/user"
+	"github.com/alonsofritz/simple-chatapp-go/server/router"
 )
 
 func main() {
-	_, err := db.NewDatabase()
+	dbConn, err := db.NewDatabase()
 	if err != nil {
 		log.Fatal("Could not initialize database connection: %s", err)
 	}
+
+	userRep := user.NewUserRepository(dbConn.GetDB())
+	userSvc := user.NewUserService(userRep)
+	userHandler := user.NewUserHandler(userSvc)
+
+	router.InitRouter(userHandler)
+	router.Start("0.0.0.0:8080")
 }
---
+++ server/router/router.go
@@ -0,0 +1,18 @@
+package router
+
+import (
+	"github.com/alonsofritz/simple-chatapp-go/server/internal/user"
+	"github.com/gin-gonic/gin"
+)
+
+var r *gin.Engine
+
+func InitRouter(userHandler *user.UserHandler) {
+	r = gin.Default()
+
+	r.POST("/signup", userHandler.CreateUser)
+}
+
+func Start(addr string) error {
+	return r.Run(addr)
+}
